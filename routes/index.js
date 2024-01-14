62dd2258b12d6699c92a5ead3ef46bcbb6631a81
Nikiticko <nikitasemenenko3@gmail.com>
2024-01-14 23:03:15 +0300

    insert img

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/Judy', function(req, res, next) {
+  res.send("<h1>Страница Judy</h1>");
 });
-
+router.get('/Johnny', function(req, res, next) {
+  res.send("<h1>Страница Johnny</h1>");
+});
+router.get('/Panam', function(req, res, next) {
+  res.send("<h1>Страница Panam</h1>");
+});
 module.exports = router;
